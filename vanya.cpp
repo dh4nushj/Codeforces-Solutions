d10b6f1d733e2d3ccd53393bd0f5a08e057b60ea
dh4nushj <dhanushj2006btw@gmail.com>
2025-03-25 22:08:36 +0530

    Update vanya.cpp

--- vanya.cpp
+++ vanya.cpp
@@ -1,10 +1,12 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
 int main(){
     int n, h;
     cin>>n>>h; // number of friends and the height of the fence
-    int width = 0, a[20];
+    int width = 0;
+    vector<int> a(n);
     for(int i =0; i<n; i++){
         cin>>a[i]; // height of the ith person
         if(a[i] > h){
